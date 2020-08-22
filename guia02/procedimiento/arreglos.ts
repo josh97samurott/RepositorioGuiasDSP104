0e338bb5eace4abee7e80c583c60bcb780d560fd
Taru <joshuacalebreyes77@gmail.com>
2020-08-22 17:35:30 -0600

    Guia02_Desarrollo (PARTE 1)

---
+++ guia02/procedimiento/arreglos.ts
@@ -0,0 +1,7 @@
+var skills:Array<String> = ['JavaScript', 'TypeScript', 'Angular'];
+
+var numberArray:number[] = [123, 123, 1213, 1231];
+
+console.log(skills);
+
+console.log(numberArray);
---
+++ guia02/procedimiento/enumerables.ts
@@ -0,0 +1,5 @@
+enum ROLE{Employee=5, Manager=4, Admin=2, Developer=1}
+
+var role:ROLE = ROLE.Employee;
+
+console.log(role);
